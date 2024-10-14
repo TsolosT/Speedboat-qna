c01fe904e976e49a68e53462ad5ca6845269bdd4
TsolovikosT <teotsolos@hotmail.gr>
2024-10-14 16:26:54 +0300

    feat: changed to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
+import Divider from '@mui/material/Divider'
 function About() {
     return (
         <Container 
@@ -26,15 +26,19 @@ function About() {
                 }}
             >
                 <CardContent>
-                    <Typography variant="h4" component="div" sx={{ mb: 2 }}>
+                    <Typography variant="h4" component="div" sx={{ mb: 1 }}>
                         SpeedBoat Exam Q&A 
                     </Typography>
-                    <Typography sx={{ color: 'secondary.main', mb: 3 }} variant="h6">
+
+                    <Typography sx={{ color: 'secondary.main'}} variant="h6">
                         Study Case
                     </Typography>
+                    <Divider aria-hidden="true" sx={{  bgcolor: 'primary.main' , mb: 3}}/>
+
                     <Typography variant="body1" sx={{ mb: 2 }}>
-                        A React application for practicing Q&A tests for speedboat exams.
+                        A Vite React application for practicing Q&A tests for speedboat exams.
                         <br />
+                        Course reference from: {' '}
                         <a 
                             style={{ color: '#56CCF2' }} 
                             href="https://sgouros-trainingboat.gr/"
@@ -42,10 +46,10 @@ function About() {
                             Sgouros Training Boat
                         </a>
                         <br />
-                        The study and test content in the current version is available in Greek.
+                        Note: The study and test content in the current version is available in Greek.
                     </Typography>
                     <Typography variant="body2" sx={{ mt: 2 }}>
-                        Version <span>1.0.0</span>
+                        Version <span>1.1.0</span>
                     </Typography>
                 </CardContent>
             </Card>
